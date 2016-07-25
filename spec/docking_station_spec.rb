0b8ca3072f494f1c402cf74d6ac197572e741961
alexfakhri <alexfakhri@me.com>
2016-07-25 18:08:06 +0100

    Added capacity of 20 to the docking station

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -27,17 +27,17 @@ describe DockingStation do
 
     it 'docks a bike' do
       bike = Bike.new
-      expect(subject.dock(bike)).to eq bike
+      expect(subject.dock(bike)).to eq [bike]
     end
 
     it 'returns docked bikes' do
       bike = Bike.new
       subject.dock(bike)
-      expect(subject.bike).to eq bike
+      expect(subject.bikes).to eq [bike]
     end
 
     it 'raises an error when full' do
-      subject.dock(Bike.new)
+      20.times { subject.dock Bike.new }
       expect{ subject.dock Bike.new }.to raise_error 'Docking station full'
     end
 
