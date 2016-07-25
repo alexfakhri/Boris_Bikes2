7d76e907490c305d31fb21574569de8d1da6f6eb
alexfakhri <alexfakhri@me.com>
2016-07-25 15:37:03 +0100

    adding unit tests to dock and release bike

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -11,4 +11,15 @@ describe DockingStation do
     expect(bike).to be_working
   end
 
+  it 'docks a bike' do
+    bike = Bike.new
+    expect(subject.dock(bike)).to eq bike
+  end
+
+  it 'returns docked bikes' do
+    bike = Bike.new
+    subject.dock(bike)
+    expect(subject.bike).to eq bike
+  end
+
 end
