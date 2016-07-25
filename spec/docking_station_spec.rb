745999a63d7468103980c2ca1a9e1ac1e863af85
alexfakhri <alexfakhri@me.com>
2016-07-25 17:03:28 +0100

    Added errors for releasing bikes that do not exist

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -2,13 +2,25 @@ require 'docking_station'
 
 describe DockingStation do
 
-  it 'responds to release_bike' do
-    expect(subject).to respond_to :release_bike
-  end
+  describe '#release_bike' do
 
-  it 'releases working bikes' do
-    bike = subject.release_bike
-    expect(bike).to be_working
+    it 'releases a Bike' do
+      bike = Bike.new
+      subject.dock(bike)
+      expect(subject.release_bike).to eq bike
+    end
+
+    it 'releases working bikes' do
+      bike = Bike.new
+      subject.dock(bike)
+      subject.release_bike
+      expect(bike).to be_working
+    end
+
+    it 'raises and erorr when there are no bikes available' do
+      expect { subject.release_bike }.to raise_error 'No bikes available'
+    end
+
   end
 
   it 'docks a bike' do
@@ -21,5 +33,6 @@ describe DockingStation do
     subject.dock(bike)
     expect(subject.bike).to eq bike
   end
+
 
 end
