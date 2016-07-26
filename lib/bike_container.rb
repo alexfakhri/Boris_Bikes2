9fb902d5ee62ba23ddd82ed2d27d8aa141bf1898
alexfakhri <alexfakhri@me.com>
2016-07-26 12:16:13 +0100

    Adding remove and add bike features to the bike container"

--- lib/bike_container.rb
+++ lib/bike_container.rb
@@ -4,8 +4,31 @@ module BikeContainer
 
   attr_reader :capacity
 
-  def initialize
-    @capacity = DEFAULT_CAPACITY
+  def initialize(capacity = DEFAULT_CAPACITY)
+    @capacity = capacity
+    @bikes = []
   end
+
+  def add_bike(bike)
+    raise "#{self.class.name} full" if full?
+    bikes << bike
+  end
+
+  def remove_bike
+    raise "#{self.class.name} empty" if empty?
+    bikes.pop
+  end
+
+  def empty?
+    bikes.empty?
+  end
+
+  def full?
+    bikes.count >= capacity
+  end
+
+  private
+
+  attr_reader :bikes
 
 end
